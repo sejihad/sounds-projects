042e918e94fdda207978ef6f0ed1ddd21cf4c245
sejihad <153150003+sejihad@users.noreply.github.com>
2024-02-04 22:10:33 +0600

    Update test.js

--- test.js
+++ test.js
@@ -4,19 +4,34 @@ for (var i = 0; i < 3; i++) {
     [i].addEventListener("click", function () {
       var text = this.innerHTML;
 
-      switch (text) {
-        case "a":
-          var audio = new Audio("./sounds/a.m4a");
-          audio.play();
-          break;
-        case "b":
-          var audio = new Audio("./sounds/b.m4a");
-          audio.play();
-          break;
-        case "c":
-          var audio = new Audio("./sounds/c.m4a");
-          audio.play();
-          break;
-      }
+      playAudio(text);
+      playAnimation(text);
     });
+}
+
+function playAudio(text) {
+  switch (text) {
+    case "a":
+      var audio = new Audio("./sounds/a.m4a");
+      audio.play();
+
+      break;
+    case "b":
+      var audio = new Audio("./sounds/b.m4a");
+      audio.play();
+      break;
+    case "c":
+      var audio = new Audio("./sounds/c.m4a");
+      audio.play();
+      break;
+  }
+}
+
+function playAnimation(text) {
+  var selected = document.querySelector("." + text);
+
+  selected.classList.add("anim");
+  setTimeout(() => {
+    selected.classList.remove("anim");
+  }, 1000);
 }
